99f0dccac352da2b4e89be1bbedccb22c9e1edd1
dlgpal95 <dlgpal9508@gmail.com>
2020-05-08 16:35:00 +0900

    1

--- Algorithm/src/Algo20200508/AddBinary.java
+++ Algorithm/src/Algo20200508/AddBinary.java
@@ -1,7 +1,12 @@
 package Algo20200508;
 
 public class AddBinary {
-	class Solution {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.addBinary("1110110101", "1110111011"));
+	}
+
+	static class Solution {
 		public String addBinary(String a, String b) {
 			int anum = Integer.parseInt(a);
 			int bnum = Integer.parseInt(b);
@@ -20,11 +25,14 @@ public class AddBinary {
 					sb.append(Integer.parseInt(sp[i]) + plus - 2);
 					plus = 1;
 				} else {
-					if (i != 0) {
-						sb.append(Integer.parseInt(sp[i]) + 1);
-						plus = 0;
+
+					if (Integer.parseInt(sp[i]) + plus == 0) {
+						sb.append(Integer.parseInt(sp[i]));
+					} else {
+						sb.append(Integer.parseInt("1"));
 					}
 
+					plus = 0;
 				}
 
 			}
@@ -35,6 +43,27 @@ public class AddBinary {
 
 			String reversed = sb.reverse().toString();
 			return reversed;
+		}
+
+	}
+
+//=============Integer Max값 생각하기.!!!!!!!!!!!! 처음부터 안되는 코드 ^^^^^^^^^^=================//
+	public class Solution2 {
+		public String addBinary(String a, String b) {
+			StringBuilder sb = new StringBuilder();
+			int i = a.length() - 1, j = b.length() - 1, carry = 0;
+			while (i >= 0 || j >= 0) {
+				int sum = carry;
+				if (j >= 0)
+					sum += b.charAt(j--) - '0';
+				if (i >= 0)
+					sum += a.charAt(i--) - '0';
+				sb.append(sum % 2);
+				carry = sum / 2;
+			}
+			if (carry != 0)
+				sb.append(carry);
+			return sb.reverse().toString();
 		}
 	}
 }
